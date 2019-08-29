38d02cf1442d210ca3e658864071987432d1e62b
Cristiano Ventura <mrk.cristiano@gmail.com>
2019-08-28 20:59:40 -0400

    Setup nodejs service and dockerfile

---
+++ nodejs-service/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({
+		hello: 'world',
+		from: 'a service written in nodejs',
+		'message sent': ''
+	});
+});
+
+app.listen('3000', () => console.log('Node.js server running'));
